1b4c592f5b3c5b27f85bd9ee841fc2833f642c10
ThatOneKid21 <54866149+ThatOneKid21@users.noreply.github.com>
2019-09-19 13:39:41 -0600

    update

--- scripts/LeasonIntro/operators.js
+++ scripts/LeasonIntro/operators.js
@@ -1,12 +1,23 @@
-//console.log("did you change my file name");
-console.log("You made it to the console");
+  //console.log("did you change my file name");
+  console.log("You made it to the console");
 
-let number = [4, 7, 13, 99, 0, 100, 5, 1, '1']
+  let number = [4, 7, 13, 99, 0, 100, 5, 1, '1'];
 
-// simpleOperators
-let adding = number[0] + number[1];
-//let subtraction
-//let multiplying
-//let division
-//let modulus
-console.log('4 + 7=', adding);
+  // simpleOperators
+  function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtraction = numberA - numberB;
+  let multiplying = numberA * numberB;
+  let division = numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA + numberB;, 'adding'adding);
+  console.log(numberA + numberB, 'subtraction'subtraction);
+  console.log(numberA + numberB, 'multiplying'multiplying);
+  console.log(numberA + numberB, 'division'division);
+  console.log(numberA + numberB,'modulus'modulus);
+}
+
+let number = [4, 7, 13, 99, 0, 100, 5, 1, '1'];
+simpleOperators (number[0], number[1]);
+simpleOperators (number[2], number[3]);
+simpleOperators (number[4], number[1]);
